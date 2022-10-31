96df09e8a4fb55a8fade4d93f90bf05045c3ba64
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2022-10-31 20:52:24 +0530

    Adding Angular Mention sample
    Adding Angular Mention sample

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import {registerLicense} from '@syncfusion/ej2-base';
+if (environment.production) {
+  enableProdMode();
+}
+registerLicense("ORg4AjUWIQA/Gnt2VVhhQlFac11JW3xNYVF2R2FJe1RzdF9DZkwgOX1dQl9hSXtTcEVhWndceXFdQmY=");
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
